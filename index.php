63819d1bbaaf8e91ae1e767d6f541fcdc2bff2e2
deyvid <deyvidmartins692@gmail.com>
2025-07-20 17:19:14 -0300

    pagina de login

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="Pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+
+        <p><input type="text" name="email" placeholder="Digite seu email:"></p>
+        <p><input type="passowrd" placeholder="Digite sua senha:"></p>
+        <p><button type="submit">Entrar</button></p>
+        <p><a href="">Esqueceu sua senha?</a></p>
+        </form>
+    <?php 
+
+
+    ?>
+    
+</body>
+</html>
