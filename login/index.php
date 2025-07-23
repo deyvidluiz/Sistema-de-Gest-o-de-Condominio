ac28e04ae73749553cc74cf440a2eba63ab3314e
deyvid <deyvidmartins692@gmail.com>
2025-07-23 11:52:33 -0300

    login

--- login/index.php
+++ login/index.php
@@ -7,15 +7,28 @@
 
         //  print_r($_POST['nome']);
         
-         include_once('c:\xampp\htdocs\Sistema_de_condominio\conexão\conexão.php'); 
+         include_once('../includes/conexao/conexao.php'); 
 
          $nome = $_POST["nome"];
          $email = $_POST['email'];
          $senha = $_POST['senha'];
-         $tipo = $_POST['tipo'];
-         $status = $_POST['status'];
+        //  $tipo = $_POST['tipo'];
+        //  $cpf = $_POST['cpf'];
 
-         $result = mysqli_query($conexao, "INSERT INTO usuarios(nome,email,senha,tipo,status) VALUES('$nome','$email','$senha','$tipo','$status')");
+         $sql_verifica = "SELECT * FROM usuarios WHERE nome = ? AND email = ? AND senha = ?";
+         $stmt = $mysqli->prepare($sql_verifica);
+         $stmt->bind_param ("sss", $nome, $email, $senha);
+         $stmt->execute();
+         $result = $stmt->get_result();
+
+         if ($result->num_rows == 3) {
+            echo"login efetuado com sucesso";
+            # co""de...
+        }else
+        {
+            echo 'Login invalido';
+            echo '<br>Cadastrar-se <a href="../acesso/cadastro.php">Clique aqui</a>';
+        }
 
 
     }
@@ -30,15 +43,13 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="index.php" method="post">
+    <<form action="index.php" method="post">
 
-        <p><input type="text" name="nome" placeholder="Digite seu nome:"></p>
-        <p><input type="email" name="email" placeholder="Digite seu email:"></p>
-        <p><input type="password" name="senha" placeholder="Digite sua senha:"></p>
-        <p><input type="text" name="tipo" placeholder="Digite seu tipo:"></p>
-        <p><input type="number" name="status" placeholder="Digite seu status:"></p>
-        <p><button type="submit" name="submit">Entrar</button></p>
-        <p><a href="">Esqueceu sua senha?</a></p>
-        </form>
+<p><label for="nome">Nome:</label> <input type="text" id="nome" name="nome" placeholder="Digite seu nome"></p>
+<p><label for="email">Email:</label> <input type="email" id="email" name="email" placeholder="Digite seu email"></p>
+<p><label for="senha">Senha:</label> <input type="password" id="senha" name="senha" placeholder="Digite sua senha"></p>
+<p><button type="submit" name="submit">Entrar</button></p>
+<p><a href="">Esqueceu sua senha?</a></p>
+</form>
 </body>
 </html>
